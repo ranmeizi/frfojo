c1eae6bd1f711bd04a9c67a64b2237df1bc434f6
boboan <360969885@qq.com>
2026-03-06 11:07:02 +0800

    feat: 试一试user

--- webs/user-center/vite.config.ts
+++ webs/user-center/vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { useDynamicPublicPath } from "vite-plugin-dynamic-publicpath";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({ jsxRuntime: "automatic" })],
+  plugins: [
+    react({ jsxRuntime: "automatic" }),
+    useDynamicPublicPath(/** option */),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
